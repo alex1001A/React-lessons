97bf6fbf903b8a0823817571873e94c17c21a214
alex1001A <alexiordanov1998@gmail.com>
2024-04-01 17:01:16 +0200

    commit 2 React HW

--- Lesson-3-HW-Three-pages-site/lesson-3-site/src/Components/Card/Card.jsx
+++ Lesson-3-HW-Three-pages-site/lesson-3-site/src/Components/Card/Card.jsx
@@ -1,20 +1,18 @@
 import useFetch from 'react-fetch-hook'
 
-
 import './Card.scss'
 
-export default function Card() {
+export default function Card({pageCount}) {
 
-    const { data } = useFetch('https://rickandmortyapi.com/api/character')
+    const apiLink = `https://rickandmortyapi.com/api/character?page=${pageCount}`
 
-    console.log(data?.results);
-    console.log(data);
+    const { data } = useFetch(apiLink)
 
     const list = data?.results.map(item => (
 
-        <div className="card">
+        <div className="card" key={item.id}>
             <h2 className='card__title'>{item.name}</h2>
-            <img className='card__img' src={item.image} alt="" />
+            <img className='card__img' src={item.image} alt={item.name} />
             <div className="card__info">
                 <h3 className='card__info_gender'>{item.gender}</h3>
                 <h3 className='card__info_species'>{item.species}</h3>
--- Lesson-3-HW-Three-pages-site/lesson-3-site/src/Pages/Home/Home-page.jsx
+++ Lesson-3-HW-Three-pages-site/lesson-3-site/src/Pages/Home/Home-page.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react"; //Деструктуризація
+
 import Card from '../../Components/Card/Card'
 
 import './Home-page.scss'
 
 export default function Home() {
+    
+    const [count, setPageCount] = useState(1);
+    const maxPageCount = 42
+
+    const nextPage = () => {
+        setPageCount(prevPageCount => Math.min(prevPageCount + 1, maxPageCount));
+    };
+
+    const prevPage = () => {
+        setPageCount(prevPageCount => Math.max(prevPageCount - 1, 1));
+    };
+
     return (
         <section className="hero">
+            <div className="button-wrap">
+                <button onClick={nextPage} className='page-button page-button_next'></button>
+                <button onClick={prevPage} className='page-button page-button_prev'></button>
+            </div>
             <div className="container hero__container">
-                <Card/>
+                <Card pageCount={count}/>
             </div>
         </section>
     )
