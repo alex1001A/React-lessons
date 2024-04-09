e5618689aa6c594d9d058075c5f251805b6f2cc1
alex1001A <alexiordanov1998@gmail.com>
2024-04-09 20:58:50 +0300

    lesson 6 GraphQl

--- Lesson-5_Reptiles/my-app/src/App.jsx
+++ Lesson-5_Reptiles/my-app/src/App.jsx
@@ -1,22 +1,57 @@
-import useFetch from 'react-fetch-hook'
-import { Route, Routes } from 'react-router-dom' //Бібліотеки
+import useFetch from "react-fetch-hook";
+import { Route, Routes } from "react-router-dom"; //Бібліотеки
+import { useQuery, gql } from "@apollo/client";
 
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import Home from './Pages/Home/Home'
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
 
-import './App.css';
+import "./App.css";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    collections {
+      meta {
+        pagination {
+          page
+          pageCount
+        }
+      }
+      data {
+        id
+        attributes {
+          title
+          description
+          favourite
+          img {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function App() {
+
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  console.log(data?.collections.data.map(item => {
+    
+  }));
+
   return (
     <div className="wrapper">
-
       <Header />
 
-      <main className='main'>
+      <main className="main">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='*' element={<h1>404</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </main>
 
@@ -24,5 +59,3 @@ export default function App() {
     </div>
   );
 }
-
-
--- Lesson-5_Reptiles/my-app/src/index.jsx
+++ Lesson-5_Reptiles/my-app/src/index.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-import App from './App';
+import App from "./App";
 
-import './style/main.scss';
+import "./style/main.scss";
 
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <ApolloProvider client={client}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-
+  </ApolloProvider>
 );
-
-
